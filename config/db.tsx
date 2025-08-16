429a6c08f8f5265d95ff93c30e41fd732301eb82
KabirSahariya <kabirsahariya786@gmail.com>
2025-08-16 17:18:12 +0530

    Modern Frontend

---
+++ config/db.tsx
@@ -0,0 +1,5 @@
+// Synchronous connection
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(process.env.DATABASE_URL!);
